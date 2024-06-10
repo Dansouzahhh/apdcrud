ae18f970233f52ac0e0709bc6bdf9057374fd7e2
Danielsouza <Dansouzahhh00200@gmail.com>
2024-06-10 17:24:56 -0300

    php-Boots

---
+++ boostcrud/foooter.php
@@ -0,0 +1,5 @@
+<?php
+<hr>
+<div class="conteiner"> &copy;2019 </div>
+</body> <html>
+?>
---
+++ boostcrud/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <title>CRUD</title>
+    <link rel="stylesheet"type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <style type="text/css">
+        .box { background: #f0f0f0; padding: 20px;}
+        <style> </head>
+        <body>
+
+        <div class="container">
+    <nav=class"navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header">
+
+    <button type="button" class="nav-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expend="false" aria-controls="navbar">
+
+    <span class="sr-only"> toggle navigation </span>
+
+    <span class="icon-bar"></span>
+
+    <span class="icon-bar"></span>
+
+    <span class="icon-bar"></span>
+
+    </button>
+
+    <a class="navbar-brand" href="index.php" >CRUD</a>
+
+</div>
+
+<div id="navbar" class="navbar-collapse collapse">
+
+<ul class="nav navbar-nav">
+
+<li class="active"><a href="index.php">Home</a></li>
+
+<li><a heref="insert.php">Add user</a></li>
+
+<li><a heref="user.php">All user</a></li>
+
+</ul>
+
+</div><!--/.nav-collapse-->
+
+</div><!--/.cointeiner-fluid-->
+
+</nav>
+
+</div>
+
+
+
+
